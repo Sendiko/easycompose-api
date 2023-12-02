29477aeec5636da63527aa60e24205c3ba6e704f
Sendiko <sarangtawon897@gmail.com>
2023-12-02 21:35:14 +0700

    enc(imp): mvc

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const router = require("./router/route");
-const PORT = 3000;
+const cors = require("cors");
+const router = require("./app/router/route");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-app.listen(PORT, () => {
-  console.log(`server is running at ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server is running at ${process.env.PORT}`);
 });
