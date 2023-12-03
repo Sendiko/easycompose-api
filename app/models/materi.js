8f23479698a2e0497c82c6a4664284316c18ffc4
Sendiko <sarangtawon897@gmail.com>
2023-12-03 16:17:23 +0700

    enc(crea): materi moidel

---
+++ app/models/materi.js
@@ -0,0 +1,29 @@
+const sequelize = require("../config/db.config");
+const DataType = require("sequelize");
+const submateri = require("../models/submateri");
+
+const materi = sequelize.define("materi", {
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataType.BIGINT,
+  },
+  judul: {
+    type: DataType.STRING,
+    allowNull: false,
+  },
+  deskripsi: {
+    type: DataType.TEXT,
+    allowNull: false,
+  },
+});
+
+materi.sync()
+  .then(() => {
+    console.log("materi table created!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+module.exports = materi;
