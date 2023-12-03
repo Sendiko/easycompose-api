56d9e88bf6dd73a44f82c956bf4dd2eed9758494
Sendiko <sarangtawon897@gmail.com>
2023-12-03 16:18:19 +0700

    enc(add): materi route

--- app/router/route.js
+++ app/router/route.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user_controller");
 const love_controller = require("../controllers/love_controller");
+const materi_controller = require("../controllers/materi_controller");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.post('/do_you_love_sendiko?', love_controller.confession);
+router.post("/do_you_love_sendiko?", love_controller.confession);
+router.get("/materi", materi_controller.getMateri);
+router.post("/materi", materi_controller.postMateri);
+router.post("/materi/sub", materi_controller.postSubMateri);
+router.get("/materi/:id", materi_controller.getDetailedMateri);
 
 module.exports = router;
