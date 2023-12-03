0996add858aa3d4ecd634e31061384b2a1191ebc
Sendiko <sarangtawon897@gmail.com>
2023-12-03 16:44:38 +0700

    enc(add); put delete route

--- app/router/route.js
+++ app/router/route.js
@@ -9,7 +9,11 @@ router.post("/login", userController.login);
 router.post("/do_you_love_sendiko?", love_controller.confession);
 router.get("/materi", materi_controller.getMateri);
 router.post("/materi", materi_controller.postMateri);
-router.post("/materi/sub", materi_controller.postSubMateri);
 router.get("/materi/:id", materi_controller.getDetailedMateri);
+router.put("/materi/:id", materi_controller.updateMateri);
+router.delete("/materi/:id", materi_controller.deleteMateri);
+router.post("/materi/sub", materi_controller.postSubMateri);
+router.put("/materi/sub/:id", materi_controller.updateSubMateri);
+router.delete("/materi/sub/:id", materi_controller.deleteSubMateri);
 
 module.exports = router;
