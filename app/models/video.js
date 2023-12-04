5caae3eae7c9975d97b7256ec63b0694e309d2ac
Sendiko <sarangtawon897@gmail.com>
2023-12-04 11:35:48 +0700

    enc(crea): model

---
+++ app/models/video.js
@@ -0,0 +1,30 @@
+const sequelize = require("../config/db.config");
+const DataType = require("sequelize");
+
+const video = sequelize.define("video", {
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    type: DataType.BIGINT
+  },
+  judul: {
+    type: DataType.STRING,
+    allowNull: false,
+  },
+  url: {
+    type: DataType.TEXT,
+    allowNull: false,
+  },
+});
+
+video
+  .sync()
+  .then(() => {
+    console.log("video table created!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+module.exports = video;
