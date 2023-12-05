6e1b69705913dc8a6a0f29e236674832dbdd67ee
Sendiko <sarangtawon897@gmail.com>
2023-12-05 15:05:25 +0700

    enc(add): necessary routes

--- app/router/route.js
+++ app/router/route.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/user_controller");
 const love_controller = require("../controllers/love_controller");
 const materi_controller = require("../controllers/materi_controller");
 const video_controller = require("../controllers/video_controller");
+const road_to_safety_controller = require("../controllers/road_to_safety_controller");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
@@ -23,4 +24,9 @@ router.post("/video", video_controller.create);
 router.put("/video/:id", video_controller.update);
 router.delete("/video/:id", video_controller.delete);
 
+router.get("/road", road_to_safety_controller.index);
+router.get("/road/:id", road_to_safety_controller.show);
+router.post("/road", road_to_safety_controller.create);
+router.put("/road/:id", road_to_safety_controller.update);
+router.delete("/road/:id", road_to_safety_controller.delete);
 module.exports = router;
